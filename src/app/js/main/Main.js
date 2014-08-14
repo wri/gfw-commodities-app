6065517a9e3e37f59ccc1377647fc3c4c9bc151b
Robert Winterbottom <rwinterbottom@blueraster.com>
2014-08-14 17:37:09 -0400

    Added some new files and css

---
+++ src/app/js/main/Main.js
@@ -0,0 +1,38 @@
+define([
+	"main/config",
+	"esri/config",
+	"dojo/_base/array",
+	// Load Necessary Layout Widgets and Parser Here
+	"dojox/mobile/parser",
+  "dijit/layout/ContentPane",
+  "dijit/layout/StackContainer"
+], function (AppConfig, esriConfig, arrayUtils, parser) {
+
+	return {
+
+		init: function () {
+
+			// Parse the initial Layout
+			parser.parse();
+
+			// Call remaining setup functions first, then launch the app
+			this.applyConfigurations();
+			this.launchApp();
+			
+		},
+
+		applyConfigurations: function () {
+
+			arrayUtils.forEach(AppConfig.corsEnabledServers, function (server) {
+				esriConfig.defaults.io.corsEnabledServers.push(server);
+			});
+
+		},
+
+		launchApp: function () {
+			alert("Hey Ohh");
+		}
+
+	};
+
+});
---
+++ src/app/js/main/config.js
@@ -0,0 +1,14 @@
+define({
+
+	"title": "Global Forest Watch Commodities",
+
+	"corsEnabledServers": [
+		"https://api-ssl.bitly.com",
+		"http://globalforestwatch.org",
+		"http://firms.modaps.eosdis.nasa.gov",
+		"http://gis-potico.wri.org",
+		"http://gfw-apis.appspot.com",
+		"http://50.18.182.188"
+	]
+
+});
