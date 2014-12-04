9c397b6438889434fbe84e2da0d25aac53a70986
Alicia Barnash <abarnash@blueraster.com>
2014-12-04 12:10:38 -0500

    Removing temporary tree cover density

--- src/app/js/report/Fetcher.js
+++ src/app/js/report/Fetcher.js
@@ -383,34 +383,34 @@ define([
                 deferred.resolve(false);
             }
 
-            /*
-                TEMPORARY HARDCODED VALUES
-                URL IS REPLACED
-                CERTAIN LAYER IDS ARE DIFFERENT
-                CHANGE BEFORE DEPLOYMENT
-                EITHER IN CONFIG AND MAKE PERMANENT OR JUST DELETE CONTENT
-                    DIRECTLY BELOW HERE TIL NEXT TEMPORARY COMMENT
-            */
+            // /*
+            //     TEMPORARY HARDCODED VALUES
+            //     URL IS REPLACED
+            //     CERTAIN LAYER IDS ARE DIFFERENT
+            //     CHANGE BEFORE DEPLOYMENT
+            //     EITHER IN CONFIG AND MAKE PERMANENT OR JUST DELETE CONTENT
+            //         DIRECTLY BELOW HERE TIL NEXT TEMPORARY COMMENT
+            // */
 
-            url = 'http://gis-potico.wri.org/arcgis/rest/services/CommoditiesAnalyzer/GFWanalysis_wm/ImageServer';
-            if (config.rootNode === 'primaryForest') {
-                config.rasterId = '$11';
-            }
-            if (config.rootNode === 'treeCoverDensity') {
-                config.rasterId = '$12';
-                config.rasterRemap = {
-                    'rasterFunction': 'Remap',
-                    'rasterFunctionArguments': {
-                        'InputRanges': [0, 11, 11, 26, 26, 51, 51, 76, 76, 101],
-                        'OutputValues': [0, 1, 2, 3, 4],
-                        'Raster': '$12',
-                        'AllowUnmatched': false
-                    }
-                };
-            }
-            /*
-                TEMPORARY CONTENT ABOVE
-            */
+            // url = 'http://gis-potico.wri.org/arcgis/rest/services/CommoditiesAnalyzer/GFWanalysis_wm/ImageServer';
+            // if (config.rootNode === 'primaryForest') {
+            //     config.rasterId = '$11';
+            // }
+            // if (config.rootNode === 'treeCoverDensity') {
+            //     config.rasterId = '$12';
+            //     config.rasterRemap = {
+            //         'rasterFunction': 'Remap',
+            //         'rasterFunctionArguments': {
+            //             'InputRanges': [0, 11, 11, 26, 26, 51, 51, 76, 76, 101],
+            //             'OutputValues': [0, 1, 2, 3, 4],
+            //             'Raster': '$12',
+            //             'AllowUnmatched': false
+            //         }
+            //     };
+            // }
+            // /*
+            //     TEMPORARY CONTENT ABOVE
+            // */
 
             // If the report analyzeClearanceAlerts is false, just resolve here
             //if (report.analyzeClearanceAlerts) {
