118276b39e44140408dcdac00b3a501e5a09763c
Carolyn Borton <carolyn@sunshinelearningsystems.com>
2014-12-17 12:23:56 -0500

    Changed MapServer url for Forest Gain tiles to 50.18.182.188:6080.

--- src/app/js/map/config.js
+++ src/app/js/map/config.js
@@ -4,7 +4,7 @@ define([], function () {
 	// The dynamicMapServiceUrl is currently being used by the following layers (by key):
 	// ifl, peat, tfcs, ldcover, legal, oilPerm, logPerm, minePerm, woodPerm
 	var dynamicMapServiceUrl = "http://gis-potico.wri.org/arcgis/rest/services/CommoditiesAnalyzer/moremaps2_EN/MapServer",
-			treeCoverGainUrl = 'http://gis-potico.wri.org/arcgis/rest/services/GFW/Forest_Gain_2000_2012_map/MapServer',
+			treeCoverGainUrl = 'http://50.18.182.188:6080/arcgis/rest/services/ForestGain_2000_2012_map/MapServer',
 			treeCoverGainImageUrl = 'http://50.18.182.188:6080/arcgis/rest/services/ForestGain_2000_2012/ImageServer',
 			//treeCoverGainUrl = "http://54.241.14.14:6080/arcgis/rest/services/Forest_Gain_2000_2012_map/MapServer",
 			//treeCoverGainImageUrl = "http://54.241.14.14:6080/arcgis/rest/services/ForestGain_2000_2012/ImageServer",
