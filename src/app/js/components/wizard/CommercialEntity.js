26c3e50031862dd38aa15ead43e52e17df414d50
Albert Marquez <amarquez@blueraster.com>
2015-01-28 12:09:57 -0500

    concession area highlighting

--- src/app/js/components/wizard/CommercialEntity.js
+++ src/app/js/components/wizard/CommercialEntity.js
@@ -8,12 +8,13 @@ define([
   "components/wizard/NestedList",
   // Other Useful Modules
   "dojo/topic",
+  "dojo/query",
   "esri/Color",
   "esri/graphic",
   "esri/graphicsUtils",
   "esri/symbols/SimpleFillSymbol",
   "esri/symbols/SimpleLineSymbol"
-], function (React, MapConfig, AnalyzerQuery, AnalyzerConfig, NestedList, topic, Color, Graphic, graphicsUtils, SimpleFillSymbol, SimpleLineSymbol) {
+], function (React, MapConfig, AnalyzerQuery, AnalyzerConfig, NestedList, topic, query, Color, Graphic, graphicsUtils, SimpleFillSymbol, SimpleLineSymbol) {
 
   var config = AnalyzerConfig.commercialEntity,
       adminSymbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
@@ -23,6 +24,7 @@ define([
   function getDefaultState() {
     return {
       nestedListData: [],
+      activeListItemValues: [],
       isLoading: false,
       selectedCommodity: config.commodityOptions[0].value
     };
@@ -69,6 +71,7 @@ define([
             'data': this.state.nestedListData,
             'click': this._commodityClicked,
             'placeholder': 'Search commercial entities...',
+            'activeListItemValues': this.state.activeListItemValues,
             'isResetting': this.props.isResetting
           })
         )
@@ -120,6 +123,14 @@ define([
         if (target.innerHTML === AnalyzerConfig.noNameField) {
           return;
         }
+
+        var newActiveListeItemValues = []; 
+        query('.wizard-list-child-item span', target.parentNode).forEach(function(element){
+          newActiveListeItemValues.push(parseInt(element.dataset.value));
+        });
+      
+        self.setState({ activeListItemValues: newActiveListeItemValues });
+
         // Takes URL and group name, group name will always be the targets innerHTML
         AnalyzerQuery.getFeaturesByGroupName(config.groupQuery, target.innerHTML).then(function (features) {
           wizardGraphicsLayer = app.map.getLayer(MapConfig.wizardGraphicsLayer.id);
@@ -139,6 +150,9 @@ define([
           }
         });
       } else if (objectId) {
+
+        self.setState({ activeListItemValues: [parseInt(target.dataset.value)] });
+
         AnalyzerQuery.getFeatureById(config.commodityQuery.url, objectId).then(function (feature) {
 
           // Add it to the map and make it the current selection, give it a label
