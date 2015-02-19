d71c9b838794b7988c4fb4e8465dc0fa76485e8d
Robert Winterbottom <rwinterbottom@blueraster.com>
2015-02-19 15:16:05 -0500

    favicon fix since chrome is not rendering favicon.ico with relative path

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,12 @@ module.exports = function(grunt) {
     'use strict';
 
     /*
-    This build file uses requirejs optimizer but cannot optimize into a single file due to the application
-    using an external report feature.  It loads a new bootloader which pulls each file it needs, to handle that
-    the app minifies the whole application into a single file but also keeps the report folder structure intact,
-    it also has two css files, one for the whole app(app.css) which contains all the .styl files code except for 
-    report.styl, report.styl maps to report.css, the js in the report folder is minified after being copied over
-  */
+        This build file uses requirejs optimizer but cannot optimize into a single file due to the application
+        using an external report feature.  It loads a new bootloader which pulls each file it needs, to handle that
+        the app minifies the whole application into a single file but also keeps the report folder structure intact,
+        it also has two css files, one for the whole app(app.css) which contains all the .styl files code except for 
+        report.styl, report.styl maps to report.css, the js in the report folder is minified after being copied over
+    */
 
     grunt.initConfig({
 
