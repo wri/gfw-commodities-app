ded0dfc36cde8402eef93a93dee07a8590d7b178
Lucas Cotner <lcotner@blueraster.com>
2015-02-20 09:49:56 -0500

    updated time slider and report to use the new wri forest cover loss data. New build to display it at v27

--- src/app/js/map/SimpleLegend.js
+++ src/app/js/map/SimpleLegend.js
@@ -1,127 +1,127 @@
-define([
-],
-function () {
+define([],
+    function() {
 
-  SimpleLegend = function(config) {
-    this.config = config;
-    this.CLASS_NAME = 'simple-legend';
-    this.onHide = function(){};
-    this.onShow = function(){};
-    this.items = [];
-    
-    return this;
-  };
+        SimpleLegend = function(config) {
+            this.config = config;
+            this.CLASS_NAME = 'simple-legend';
+            this.onHide = function() {};
+            this.onShow = function() {};
+            this.items = [];
 
-  SimpleLegend.prototype.init = function (callback) {
+            return this;
+        };
 
-    var legendRequest = new XMLHttpRequest(),
-        layerIndex = 0,
-        content = '',
-        configLayerIds,
-        response,
-        responseLayers,
-        config = this.config,
-        self = this;
+        SimpleLegend.prototype.init = function(callback) {
 
-    legendRequest.onreadystatechange = function (res) {
-        if (legendRequest.readyState === 4) {
-            if (legendRequest.status === 200) {
-                response = JSON.parse(legendRequest.response);
+            var legendRequest = new XMLHttpRequest(),
+                layerIndex = 0,
+                content = '',
+                configLayerIds,
+                response,
+                responseLayers,
+                config = this.config,
+                self = this;
 
-
-                // Root div setup
-                content += '<div id="' + config.id + '" class="' + self.CLASS_NAME + '">';
-
-                if (config.title) {
-                  content += '<div id="' + config.id + '-title" class="simple-legend-title">' + config.title + '</div>';
-                }
-
-                // Legend items
-                configLayerIds = config.layers.map(function(layer) {
-                  return layer.id
-                });
+            legendRequest.onreadystatechange = function(res) {
+                if (legendRequest.readyState === 4) {
+                    if (legendRequest.status === 200) {
+                        response = JSON.parse(legendRequest.response);
 
 
-                responseLayers = response.layers.filter(function(responseLayer) {
-                  return configLayerIds.indexOf(responseLayer.layerId) > -1;
-                });
+                        // Root div setup
+                        content += '<div id="' + config.id + '" class="' + self.CLASS_NAME + '">';
 
-                responseLayers.forEach(function(responseLayer) {
+                        if (config.title) {
+                            content += '<div id="' + config.id + '-title" class="simple-legend-title">' + config.title + '</div>';
+                        }
 
-                  responseLayer.config = config.layers.filter(function(config) {
-                    return config.id === responseLayer.layerId;
-                  }, this)[0];
+                        // Legend items
+                        configLayerIds = config.layers.map(function(layer) {
+                            return layer.id
+                        });
 
-                  responseLayer.legend.forEach(function(legendItem, i){
-                    var itemId = config.id + '-layer-' + layerIndex + '-' + i,
-                        itemLabel = legendItem.label;
 
-                    self.items.push(itemId);
+                        responseLayers = response.layers.filter(function(responseLayer) {
+                            return configLayerIds.indexOf(responseLayer.layerId) > -1;
+                        });
 
-                    // Label overrides
-                    if (config.layers[layerIndex].labels !== undefined && config.layers[layerIndex].labels[i]) {
-                      itemLabel = config.layers[layerIndex].labels[i];
+                        responseLayers.forEach(function(responseLayer) {
+
+                            responseLayer.config = config.layers.filter(function(config) {
+                                return config.id === responseLayer.layerId;
+                            }, this)[0];
+
+                            responseLayer.legend.forEach(function(legendItem, i) {
+                                var itemId = config.id + '-layer-' + layerIndex + '-' + i,
+                                    itemLabel = legendItem.label;
+
+                                self.items.push(itemId);
+
+                                // Label overrides
+                                if (config.layers[layerIndex].labels !== undefined && config.layers[layerIndex].labels[i]) {
+                                    itemLabel = config.layers[layerIndex].labels[i];
+                                }
+
+                                content += '<div id="' + itemId + '" class="simple-legend-layer">' +
+                                    '<img class="simple-legend-image" src="data:image/png;base64,' + legendItem.imageData + '" height="' + legendItem.height + 'px" width="' + legendItem.width + 'px">' +
+                                    '<span class="simple-legend-label">' + itemLabel + '</span>' +
+                                    '</div>';
+                            });
+
+                            layerIndex += 1;
+
+                        });
+
+                        // Root div cleanup
+                        if (config.title) {
+                            content += '</div>';
+                        }
+
+                        content += '</div>';
+
+                        document.getElementById(config.parentId).insertAdjacentHTML('afterbegin', content);
+
+                        if (callback) callback.bind(self)();
+
+                    } else {
+                        console.log('Error');
                     }
-
-                    content += '<div id="' + itemId + '" class="simple-legend-layer">' +
-                                '<img class="simple-legend-image" src="data:image/png;base64,' + legendItem.imageData + '" height="' + legendItem.height + 'px" width="' + legendItem.width + 'px">' + 
-                                '<span class="simple-legend-label">' + itemLabel + '</span>' + 
-                                '</div>';
-                  });
-
-                  layerIndex += 1;
-
-                });
-
-                // Root div cleanup
-                if (config.title) {
-                  content += '</div>';
                 }
+            };
 
-                content += '</div>';
+            legendRequest.addEventListener('error', function(error) {
+                if (error) console.log(error);
+            }, false);
 
-                document.getElementById(config.parentId).insertAdjacentHTML('afterbegin',content);
+            legendRequest.open('GET', config.url, true);
+            legendRequest.send();
 
-                if (callback) callback.bind(self)();
-
-            } else {
-              console.log('Error');
-            }
         }
-    };
 
-    legendRequest.addEventListener('error', function (error) {
-      if (error) console.log(error);
-    }, false);
+        SimpleLegend.prototype.show = function() {
+            document.getElementById(this.config.id).style.display = 'block';
+            this.onShow();
+            return this;
+        };
 
-    legendRequest.open('GET', config.url, true);
-    legendRequest.send();
+        SimpleLegend.prototype.hide = function() {
+            // console.log(this.config.id);
+            document.getElementById(this.config.id).style.display = 'none';
+            this.onHide();
+            return this;
+        };
 
-  }  
+        SimpleLegend.prototype.filterItem = function(index) {
+            this.items.forEach(function(elementId, elementIndex) {
+                if (elementIndex === index) {
+                    document.getElementById(elementId).style.display = 'block';
+                } else {
+                    document.getElementById(elementId).style.display = 'none';
+                }
+            });
+            return this;
+        };
 
-  SimpleLegend.prototype.show = function () {
-    document.getElementById(this.config.id).style.display = 'block';
-    this.onShow();
-    return this;
-  };
+        return SimpleLegend;
 
-  SimpleLegend.prototype.hide = function () {
-    document.getElementById(this.config.id).style.display = 'none';
-    this.onHide();
-    return this;
-  };
-
-  SimpleLegend.prototype.filterItem = function (index) {
-    this.items.forEach(function(elementId, elementIndex) {
-      if (elementIndex === index) {
-        document.getElementById(elementId).style.display = 'block';
-      } else {
-        document.getElementById(elementId).style.display = 'none';
-      }
     });
-    return this;
-  };
-
-  return SimpleLegend;
-
-});
