3eabaf960c3187bb5fcfa0cbd852e1a7f2207606
a-marquez <amarquez@outlook.com>
2015-05-19 13:59:34 -0400

    cleaned up old AlertsDialog code in AlertsForm

--- www/src/app/js/components/alertsForm/AlertsForm.js
+++ www/src/app/js/components/alertsForm/AlertsForm.js
@@ -31,25 +31,12 @@ define([
       TEXT = AlertsConfig.TEXT,
       KEYS = AlertsConfig.STORE_KEYS,
       getDefaultState,
-      pbVal,
-      pbId1,
-      pbId2,
-      formaId = _.uniqueId(),
-      firesId = _.uniqueId(),
-      emailId = _.uniqueId(),
-      subscriptionNameId = _.uniqueId(),
-      modal,
-      formaChecked = false,
-      firesChecked = false,
-      subscriptionName = '',
-      email = '',
       self = this;
 
   getDefaultState = function () {
     return {
       features: WizardStore.get(KEYS.customFeatures),
-      selectedFeatures: WizardStore.get(KEYS.selectedCustomFeatures),
-      modalOpen: false
+      selectedFeatures: WizardStore.get(KEYS.selectedCustomFeatures)
     }
   }
 
@@ -81,21 +68,10 @@ define([
       }
     },
 
-    componentDidUpdate: function (prevProps, prevState) {
-      if (prevState.modalOpen === true) {
-        this._renderModal()
-      }
-    },
-
     render: function () {
       var currentFeatures = WizardStore.get(KEYS.selectedCustomFeatures),
           currentSelectionLabel = currentFeatures.length > 0 ? currentFeatures.map(function (feature) {return feature.attributes.WRI_label}).join(',') : TEXT.noSelection,
           self = this;
-
-      pbId1 = 'pb_' + _.random(1,100).toString();
-      pbId2 = 'pb_' + _.random(1,100).toString();
-      // Set default once because React doesn't update defaultValue
-      pbVal = pbVal || pbId1 + pbId2;
 
       return (
         React.DOM.div({className: 'relative fill'},
@@ -124,140 +100,10 @@ define([
           React.DOM.div({className:'alerts-form__footer'}, 
             React.DOM.div({className:'inline-block padding__left'}, TEXT.selection),
             React.DOM.div({className:'alerts-form__footer__selection absolute inline-block padding__wide text-gold ellipsis border-box', title:currentSelectionLabel}, currentSelectionLabel),
-            // React.DOM.button({className:'text-white back-orange no-border fill__long pointer absolute no-right no-top', onClick:this._subscribeToAlerts, disabled:(this.state.modalOpen || this.state.selectedFeatures.length === 0)}, TEXT.subscribe)
-            React.DOM.button({className:'text-white back-orange no-border fill__long pointer absolute no-right no-top', onClick:this._subscribeToAlerts}, TEXT.subscribe)
+            React.DOM.button({className:'text-white back-orange no-border fill__long pointer absolute no-right no-top', onClick:this._subscribeToAlerts, disabled:(this.state.selectedFeatures.length === 0)}, TEXT.subscribe)
           )
         )
       );
-    },
-
-    _modal: function () {
-      // TODO: refactor into own component
-      var radiusSelect,
-          modalMount = document.getElementById(AlertsConfig.MODAL_ID),
-          selectedFeatures = this.state.selectedFeatures,
-          selectionContainsPoint = _.find(selectedFeatures, function (feature) {return feature.geometry.type === 'point'}) ? true : false,
-          self = this,
-          close = function () {
-            domClass.toggle(AlertsConfig.MODAL_ID, 'active');
-            subscriptionName = '';
-            self.setState({modalOpen:false});
-            setTimeout(function () {React.unmountComponentAtNode(modalMount)}, 750);
-          },
-          emailChange = function (event) {
-            email = event.target.value;
-            self._renderModal();
-          },
-          formaChange = function (event) {
-            formaChecked = event.target.checked;
-            self._renderModal();
-          },
-          firesChange = function (event) {
-            firesChecked = event.target.checked;
-            self._renderModal();
-          },
-          subscribeClick = function (event) {
-            validations = [
-              // [!validate.isEmailAddress(emailAddr), messagesConfig.invalidEmail],
-              // [!formaCheck && !firesCheck, messagesConfig.noSelection],
-              // [!formaCheck && !firesCheck, messagesConfig.noSelection],
-              // [!subscriptionName || subscriptionName.length === 0, messagesConfig.noSelectionName],
-              // [selectedFeatures.length === 0, messagesConfig.noAreaSelection]
-            ].filter(function (validation) {
-              return validation[0];
-            }).map(function (validation) {
-              return validation[1];
-            }).join('\n');
-
-            if (validations.length > 0) {
-              alert(AlertsConfig.messagesLabel + validations);
-            } else {
-              // Map feature geometries to new Polygons for SpatialReference for union
-              // buffer points to circles if exist
-              polygons = selectedFeatures.map(function (feature) {
-                return feature.geometry.type === 'point' ? GeoHelper.preparePointAsPolygon(feature) : feature;
-              }).map(function (feature) {
-                return new Polygon(GeoHelper.getSpatialReference()).addRing(feature.geometry.rings[feature.geometry.rings.length - 1]);;
-              });
-              GeoHelper.union(polygons).then(function (unionedPolygon) {
-                debugger;
-                // if (firesCheck) {
-                //   subscriptions.push(self._subscribeToFires(unionedPolygon, subscriptionName, emailAddr));
-                // }
-                // if (formaCheck) {
-                //   subscriptions.push(self._subscribeToForma(GeoHelper.convertGeometryToGeometric(unionedPolygon), subscriptionName, emailAddr));
-                // }
-
-                // all(subscriptions).then(function (responses) {
-                //   alert(responses.join('\n'))
-                // });
-              });
-            }
-
-          };
-
-      if (selectedFeatures.length == 1) {
-        subscriptionName = selectedFeatures[0].attributes.WRI_label ;
-      }
-
-      if (selectionContainsPoint) {
-        radiusSelect = React.DOM.div({className:'margin__bottom'}, 
-          React.DOM.span({className:'margin__left'}, 'Buffer size:'),
-          React.DOM.select({className:'margin__left'},
-            TEXT.bufferOptions.map(function (option) {
-              return React.DOM.option({value:option[0]}, option[1]);
-            })
-          )
-        );
-      }
-
-      return (
-        React.DOM.div(null,
-          React.DOM.div({className:'close-icon', onClick:close}),
-          React.DOM.div({className:'modal-content'},
-            React.DOM.div({'className':'alerts-form__form no-wide border-box'},
-              React.DOM.div({className:'modal-header'}, TEXT.modalTitle),
-              React.DOM.div({className:'margin__bottom'},
-                React.DOM.input({className:'vertical-middle', type: 'checkbox', checked:formaChecked, onChange:formaChange, id:formaId}),
-                React.DOM.label({className:'vertical-middle', htmlFor:formaId}, TEXT.forma)
-              ),
-              React.DOM.div({className:'margin__bottom'},
-                React.DOM.input({className:'vertical-middle', type: 'checkbox', checked:firesChecked, onChange:firesChange, id:firesId}),
-                React.DOM.label({className:'vertical-middle', htmlFor:firesId}, TEXT.fires)
-              ),
-              React.DOM.div({className:'pooh-bear text-center'},
-                React.DOM.div({className:'pooh-bear'}, 'Please leave this blank'),
-                React.DOM.input({id:pbId1, className:'pooh-bear', type:'text', name:'name'})
-              ),
-              radiusSelect,
-              React.DOM.div({className:'text-center margin__bottom'},
-                React.DOM.input({id:subscriptionNameId, className:'border-medium-gray border-radius', type:'text', defaultValue:subscriptionName, placeholder:TEXT.subscriptionPlaceholder})
-              ),
-              React.DOM.div({className:'text-center margin__bottom'},
-                React.DOM.input({id:emailId, className:'border-medium-gray border-radius', type:'text', value:email, onChange:emailChange, placeholder:TEXT.emailPlaceholder})
-              ),
-              React.DOM.div({className:'pooh-bear text-center'},
-                React.DOM.div({className:'pooh-bear'}, 'Please do not change this field'),
-                React.DOM.input({id:pbId2, className:'pooh-bear', type:'text', name:'address', defaultValue:pbVal})
-              ),
-              React.DOM.div({className:'text-right margin__bottom'},
-                React.DOM.button({className:'text-white back-orange no-border border-radius font-16px',  onClick:subscribeClick}, TEXT.subscribe)
-              )
-            )
-          )
-        )
-      )
-    },
-
-    _renderModal: function () {
-      var modalMount = document.getElementById(AlertsConfig.MODAL_ID);
-      modal = modal || new this._modal();
-      
-      if (!modalMount) {
-        throw new Error('Undefined Error: Could not find modalMount element.');
-      } else {
-        React.renderComponent(this._modal(), modalMount);
-      }
     },
 
     _toggle: function () {
@@ -302,8 +148,8 @@ define([
       }
 
       var id = GeoHelper.nextCustomFeatureId(),
-        attrs = { "WRI_ID": id },
-        feature = new Graphic(evt.geometry, Symbols.getPolygonSymbol(), attrs);
+          attrs = { "WRI_ID": id },
+          feature = new Graphic(evt.geometry, Symbols.getPolygonSymbol(), attrs);
 
       attrs[AlertsConfig.stepTwo.labelField] = "ID - " + id + ": Custom drawn feature";
 
@@ -314,117 +160,10 @@ define([
       dojoQuery(".drawing-tools .drawing-tool-button").forEach(function (node) {
         domClass.remove(node, "active");
       });
-    }, 
-
-    _subscribeToFires: function (unionedPolygons, subscriptionName, email) {
-      var deferred = new Deferred(),
-          messagesConfig = AlertsConfig.messages,
-          firesConfig = AlertsConfig.requests.fires,
-          url = firesConfig.url,
-          options = _.cloneDeep(firesConfig.options);
-
-      options.data.features = JSON.stringify({
-        rings: unionedPolygons.rings,
-        spatialReference: unionedPolygons.spatialReference
-      });
-      options.data.msg_addr = email;
-      options.data.area_name = subscriptionName;
-      xhr(url, options).then(function (response) {
-        deferred.resolve((response.message && response.message === firesConfig.successMessage) ? messagesConfig.fireSuccess : messagesConfig.fireFail);
-      });
-      return deferred.promise;
     },
 
-    _subscribeToForma: function (geoJson, subscriptionName, email) {
-      var deferred = new Deferred(),
-          messagesConfig = AlertsConfig.messages,
-          url = AlertsConfig.requests.forma.url,
-          options = _.cloneDeep(AlertsConfig.requests.forma.options),
-          data = JSON.stringify({
-            topic: options.data.topic,
-            email: email,
-            geom: '{"type": "' + geoJson.type + '", "coordinates":[' + JSON.stringify(geoJson.geom) + ']}'
-          }),
-          request = new XMLHttpRequest()
-          self = this;
-
-      request.onreadystatechange = function () {
-        if (request.readyState === 4) {
-          deferred.resolve((JSON.parse(request.response).subscribe) ? messagesConfig.formaSuccess : messagesConfig.formaFail);
-        }
-      };
-      request.addEventListener('error', function () {
-        deferred.resolve(false);
-      }, false);
-      request.open(options.method, url, true);
-      request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-      request.send(data);
-      return deferred.promise;
-    },
-
-    // Adapted from Generator.js:subscribeToAlerts, some required functionality
-    // separated to GeoHelper & subfunctions above
     _subscribeToAlerts: function () {
-      WizardStore.set(KEYS.alertsDialogActive, true);
-
-      return
-
-      // Honeypot
-      // if ((dom.byId(pbId1) == null || dom.byId(pbId2) == null) || (dom.byId(pbId1).value.length > 0 || dom.byId(pbId2).value !== pbVal)) {
-      //   return;
-      // }
-
-      this._renderModal();
-      domClass.toggle(AlertsConfig.MODAL_ID, 'active');
-      this.setState({modalOpen:true});
-
-      return
-
-      var selectedFeatures = this.state.selectedFeatures,
-          polygons,
-          emailAddr = dom.byId(emailId).value,
-          subscriptionName = dom.byId(subscriptionNameId).value.trim(),
-          formaCheck = dom.byId(formaId).checked,
-          firesCheck = dom.byId(firesId).checked,
-          messagesConfig = AlertsConfig.messages,
-          validations = [],
-          subscriptions = [],
-          self = this;
-
-      validations = [
-        [!validate.isEmailAddress(emailAddr), messagesConfig.invalidEmail],
-        [!formaCheck && !firesCheck, messagesConfig.noSelection],
-        [!formaCheck && !firesCheck, messagesConfig.noSelection],
-        [!subscriptionName || subscriptionName.length === 0, messagesConfig.noSelectionName],
-        [selectedFeatures.length === 0, messagesConfig.noAreaSelection]
-      ].filter(function (validation) {
-        return validation[0];
-      }).map(function (validation) {
-        return validation[1];
-      }).join('\n');
-
-      if (validations.length > 0) {
-        alert(AlertsConfig.messagesLabel + validations);
-      } else {
-        // Map feature geometries to new Polygons for SpatialReference for union
-        polygons = selectedFeatures.map(function (feature) {
-          // TODO: handle circles, convert to polys (@Generator.js:177)
-          // TODO: handle points, convert to buffered circles (@Generator.js:185)
-          return new Polygon(GeoHelper.getSpatialReference()).addRing(feature.geometry.rings[feature.geometry.rings.length - 1]);;
-        });
-        GeoHelper.union(polygons).then(function (unionedPolygon) {
-          if (firesCheck) {
-            subscriptions.push(self._subscribeToFires(unionedPolygon, subscriptionName, emailAddr));
-          }
-          if (formaCheck) {
-            subscriptions.push(self._subscribeToForma(GeoHelper.convertGeometryToGeometric(unionedPolygon), subscriptionName, emailAddr));
-          }
-
-          all(subscriptions).then(function (responses) {
-            alert(responses.join('\n'))
-          });
-        });
-      }
+      WizardStore.set(KEYS.alertsDialogActive, WizardStore.get(KEYS.alertsDialogActive));
     }
   });
 
--- www/src/app/js/components/alertsForm/config.js
+++ www/src/app/js/components/alertsForm/config.js
@@ -13,7 +13,6 @@ define([
       uploadLabel: AnalyzerConfig.customArea.uploadLabel
     },
     STORE_KEYS: AnalyzerConfig.STORE_KEYS,
-
     // Unique configs
     MODAL_ID: 'subscription-modal',
     TEXT: {
@@ -21,59 +20,7 @@ define([
       selection: 'Current selection:',
       noSelection: 'none',
       modalTitle: 'Subscribe to Alerts',
-      subscribe: 'Subscribe',
-      subscriptionPlaceholder: 'Subscription name',
-      emailPlaceholder: 'youremail@example.com',
-      forma: 'Monthly Clearance Alerts',
-      fires: 'Fire Alerts',
-      bufferOptions: [
-        ['50','50km'],
-        ['40','40km'],
-        ['30','30km'],
-        ['20','20km'],
-        ['10','10km']
-      ]
-    },
-    messagesLabel: 'Please fill in the following:\n',
-    messages: {
-      invalidEmail: 'You must provide a valid email in the form.',
-      noAreaSelection: 'You must select at least one area from the list.',
-      noSelection: 'You must select at least one checkbox from the form.',
-      noSelectionName: 'You must provide a name for your subscription area.',
-      formaSuccess: 'Thank you for subscribing to Forma Alerts.  You should receive a confirmation email soon.',
-      formaFail: 'There was an error with your request to subscribe to Forma alerts.  Please try again later.',
-      fireSuccess: 'Thank you for subscribing to Fires Alerts.  You should receive a confirmation email soon.',
-      fireFail: 'There was an error with your request to subscribe to Fires alerts.  Please try again later.'
-    },
-    requests: {
-      fires: {
-        url: 'https://gfw-fires.wri.org/subscribe_by_polygon',
-        options: {
-          method: 'POST',
-          handleAs: 'json',
-          headers: {
-            'X-Requested-With': null
-          },
-          data: {
-            msg_type: 'email',
-            msg_addr: null,
-            area_name: null,
-            features: null
-          }
-        },
-        successMessage: 'subscription successful'
-      },
-      forma: {
-        url: 'http://gfw-apis.appspot.com/subscribe',
-        options: {
-          method: 'POST',
-          data: {
-            topic: 'updates/forma',
-            geom: null,
-            email: null
-          }
-        }
-      }
+      subscribe: 'Subscribe'
     }
   }
 });
