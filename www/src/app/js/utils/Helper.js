5f6b3a46a3aee637df1786164167e1affae3749a
a-marquez <amarquez@outlook.com>
2015-05-19 14:27:15 -0400

    improved map toggling animation

--- www/src/app/js/utils/Helper.js
+++ www/src/app/js/utils/Helper.js
@@ -6,8 +6,9 @@ define([
   "dojo/_base/window",
   "dojo/_base/connect",
   "dojo/_base/fx",
-	"dojo/fx"
-], function (domClass, domGeom, WizardHelper, AlertsHelper, win, connect, Fx, coreFx) {
+  "dojo/fx",
+	"dojo/on"
+], function (domClass, domGeom, WizardHelper, AlertsHelper, win, connect, Fx, coreFx, on) {
 	'use strict';
 
   var _mapContainer,
@@ -16,17 +17,29 @@ define([
       WIZARD_WIDTH = 460,
       self = this;
 
-  _getMapAnimation = function (leftAnimationValue, resize) {
+  _getMapAnimation = function (leftAnimationValue, resize, originalCenterPoint) {
     _mapContainer = _mapContainer || document.getElementById('map-container')
-    var originalCenterPoint = app.map.extent.getCenter(),
-        resize = resize || true,
-        onEnd = resize ? function () { app.map.resize(true); app.map.centerAt(originalCenterPoint) } : function () {},
+    // var originalCenterPoint = app.map.extent.getCenter(),
+    var resize = resize || true,
+        beforeBegin = function () {
+          if (resize === true) {
+            on.once(app.map, 'resize', function () {
+              app.map.centerAt(originalCenterPoint);
+            });
+          }
+        },
+        onEnd = function () {
+          if (resize === true) {
+            app.map.resize(true);
+          }
+        },
         animation = {
           node:_mapContainer,
           properties: {
             left: leftAnimationValue
           },
           duration: ANIMATION_DURATION,
+          beforeBegin: beforeBegin,
           onEnd: onEnd
         };
     return Fx.animateProperty(animation);
@@ -42,27 +55,29 @@ define([
   		}
   	},
     toggleWizard: function () {
-      var preAnimation;
+      var preAnimation,
+          originalCenterPoint = app.map.extent.getCenter();
       if (AlertsHelper.isOpen() === true) {
         preAnimation = coreFx.combine([AlertsHelper.toggleAlertsForm()].concat([_getMapAnimation(0, false)]));
         connect.connect(preAnimation, 'onEnd', function() {
-          coreFx.combine(WizardHelper.toggleWizard().concat(_getMapAnimation(WizardHelper.isOpen() ? WIZARD_WIDTH : 0))).play();
+          coreFx.combine(WizardHelper.toggleWizard().concat(_getMapAnimation(WizardHelper.isOpen() ? WIZARD_WIDTH : 0, true, originalCenterPoint))).play();
         });
         preAnimation.play();
       } else {
-        coreFx.combine(WizardHelper.toggleWizard().concat(_getMapAnimation(WizardHelper.isOpen() ? WIZARD_WIDTH : 0))).play();
+        coreFx.combine(WizardHelper.toggleWizard().concat(_getMapAnimation(WizardHelper.isOpen() ? WIZARD_WIDTH : 0, true, originalCenterPoint))).play();
       }
     },
     toggleAlerts: function () {
-      var preAnimation;
+      var preAnimation,
+          originalCenterPoint = app.map.extent.getCenter();
       if (WizardHelper.isOpen() === true) {
         preAnimation = coreFx.combine(WizardHelper.toggleWizard().concat([_getMapAnimation(0, false)]));
         connect.connect(preAnimation, 'onEnd', function() {
-          coreFx.combine([AlertsHelper.toggleAlertsForm(), _getMapAnimation(AlertsHelper.isOpen() ? 0 : WIZARD_WIDTH)]).play();
+          coreFx.combine([AlertsHelper.toggleAlertsForm(), _getMapAnimation(AlertsHelper.isOpen() ? 0 : WIZARD_WIDTH, true, originalCenterPoint)]).play();
         });
         preAnimation.play();
       } else {
-        coreFx.combine([AlertsHelper.toggleAlertsForm(), _getMapAnimation(AlertsHelper.isOpen() ? 0 : WIZARD_WIDTH)]).play();
+        coreFx.combine([AlertsHelper.toggleAlertsForm(), _getMapAnimation(AlertsHelper.isOpen() ? 0 : WIZARD_WIDTH, true, originalCenterPoint)]).play();
       }
     }
   }
