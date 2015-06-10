af5f6373dc315c6768b615be05e69187229cbbdf
Robert Winterbottom <rwinterbottom@blueraster.com>
2015-06-10 16:37:58 -0400

    updated version number

--- www/src/app/bootloader.js
+++ www/src/app/bootloader.js
@@ -1,7 +1,7 @@
 /* global window, document, location */
 (function(win, doc) {
     'use strict';
-    var version = "2.3.6",
+    var version = "2.3.8",
         URL = location.pathname.replace(/\/[^/]+$/, "") + 'app',
         dojoConfig = {
             parseOnLoad: false,
