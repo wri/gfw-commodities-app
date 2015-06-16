bf407210cbf21fb68763bba35af3ecfb8b1ccdbd
Robert Winterbottom <rwinterbottom@blueraster.com>
2015-06-16 14:26:47 -0400

    incremented version number

--- www/src/app/bootloader.js
+++ www/src/app/bootloader.js
@@ -1,7 +1,7 @@
 /* global window, document, location */
 (function(win, doc) {
     'use strict';
-    var version = "2.4.0",
+    var version = "2.4.1",
         URL = location.pathname.replace(/\/[^/]+$/, "") + 'app',
         dojoConfig = {
             parseOnLoad: false,
