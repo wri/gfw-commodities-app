fe8b581a4577054fdc130d4ba9c7360747ba4dc3
Lucas Cotner <lcotner@blueraster.com>
2015-12-03 17:17:26 -0500

    testing heroku put again..

--- www/build/app/php/post_file_to_s3.php
+++ www/build/app/php/post_file_to_s3.php
@@ -3,11 +3,13 @@ require('../../../../vendor/autoload.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   try{
+      print_r('Here');
       $s3 = Aws\S3\S3Client::factory();
-      $file = $_POST["file"];
+      $file = $_FILES['file'];
       $dataFileName = $_POST["dataFileName"];
       $dataFileType = $_POST["dataFileType"];
       $bucket=getenv('bucket');
+      
       print_r($file['name']);
       print_r($file['tmp_name']);
       // $result = $s3->putObject(array(
