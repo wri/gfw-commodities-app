ae588f94be5cc42c50a57f226973e4961275480e
Lucas Cotner <lcotner@blueraster.com>
2015-12-03 18:59:33 -0500

    Serializing php reponse into json again........

--- www/build/app/php/post_file_to_s3.php
+++ www/build/app/php/post_file_to_s3.php
@@ -1,31 +1,31 @@
 <?php
-// require('../../../../vendor/autoload.php');
-//
-// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-//   try{
-//       $s3 = Aws\S3\S3Client::factory();
-//       print_r('finally');
-//       // $bucket=getenv('bucket');
-//       // $uploads = array();
-//       // if(isset($_FILES['dataFile'])){
-//       //   $upload = json_encode(uploadFile($bucket, $_FILES['dataFile']));
-//       //   array_push($upload,$uploads);
-//       //   print_r($upload);
-//       // }
-//       // if(isset($_FILES['attributeFile'])){
-//       //   $upload2 = json_encode(uploadFile($bucket, $_FILES['attributeFile']));
-//       //   array_push($upload2,$uploads);
-//       }
-//
-//   } catch (S3Exception $e) {
-//       echo "ERROR" . $e->getMessage() . "\n";
-//   }
-// } else {
-//   echo("Sorry, you're not allowed to do this.");
-// }
-//
+require('../../../../vendor/autoload.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  try{
+      $s3 = Aws\S3\S3Client::factory();
+      print_r('finally');
+      // $bucket=getenv('bucket');
+      // $uploads = array();
+      // if(isset($_FILES['dataFile'])){
+      //   $upload = json_encode(uploadFile($bucket, $_FILES['dataFile']));
+      //   array_push($upload,$uploads);
+      //   print_r($upload);
+      // }
+      // if(isset($_FILES['attributeFile'])){
+      //   $upload2 = json_encode(uploadFile($bucket, $_FILES['attributeFile']));
+      //   array_push($upload2,$uploads);
+      }
+
+  } catch (S3Exception $e) {
+      echo "ERROR" . $e->getMessage() . "\n";
+  }
+} else {
+  echo("Sorry, you're not allowed to do this.");
+}
+// 
 // function uploadFile($bucket,$file){
 //     $upload = $s3->upload($bucket, $file['name'], fopen($file['tmp_name'], 'rb'), 'public-read');
 // }
-print_r('finally');
+
 ?>
