32071939386e59d09e68d10c02a46fd33537188c
Lucas Cotner <lcotner@blueraster.com>
2015-12-03 18:53:58 -0500

    Serializing php reponse into json again......

--- www/build/app/php/post_file_to_s3.php
+++ www/build/app/php/post_file_to_s3.php
@@ -1,20 +1,20 @@
 <?php
 require('../../../../vendor/autoload.php');
-$bucket=getenv('bucket');
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   try{
       $s3 = Aws\S3\S3Client::factory();
-
-      $uploads = array();
-      if(isset($_FILES['dataFile'])){
-        print_r('sss')
-        $upload = json_encode(uploadFile($bucket, $_FILES['dataFile']));
-        array_push($upload,$uploads);
-        print_r($upload);
-      }
-      if(isset($_FILES['attributeFile'])){
-        $upload2 = json_encode(uploadFile($bucket, $_FILES['attributeFile']));
-        array_push($upload2,$uploads);
+      print_r('finally');
+      // $bucket=getenv('bucket');
+      // $uploads = array();
+      // if(isset($_FILES['dataFile'])){
+      //   $upload = json_encode(uploadFile($bucket, $_FILES['dataFile']));
+      //   array_push($upload,$uploads);
+      //   print_r($upload);
+      // }
+      // if(isset($_FILES['attributeFile'])){
+      //   $upload2 = json_encode(uploadFile($bucket, $_FILES['attributeFile']));
+      //   array_push($upload2,$uploads);
       }
 
   } catch (S3Exception $e) {
