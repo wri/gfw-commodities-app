d01c7bf47d55a658474b091d73d765477926d34a
Lucas Cotner <lcotner@blueraster.com>
2015-12-03 17:08:45 -0500

    testing heroku put

--- www/build/app/php/post_file_to_s3.php
+++ www/build/app/php/post_file_to_s3.php
@@ -1,19 +1,29 @@
 <?php
 require('../../../../vendor/autoload.php');
+$s3 = Aws\S3\S3Client::factory();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-  echo($_ENV["bucket"]);
-// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-  // $s3 = Aws\S3\S3Client::factory();
   $file = $_POST["file"];
   $dataFileName = $_POST["dataFileName"];
   $dataFileType = $_POST["dataFileType"];
-
   $bucket=getenv('bucket');
-  echo($bucket);
+try{
+  print_r($file['name']);
+  print_r($file['tmp_name']);
+  $result = $s3->putObject(array(
+    'Bucket' => $bucket,
+    'Key'    => $file['name'],
+    'Body'   => $file['tmp_name'],
+    'ACL'    => 'public-read'
+  ));
+}catch (S3Exception $e) {
+    echo $e->getMessage() . "\n";
+}
 
-// } else {
-//   echo("Sorry, you're not allowed to do this.");
-// }
+  // $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+
+} else {
+  echo("Sorry, you're not allowed to do this.");
+}
 
 ?>
