22a1d38d40d79b845a628ec5fb0a4edf72165b1e
Lucas Cotner <lcotner@blueraster.com>
2015-12-03 17:13:36 -0500

    testing heroku put again

--- www/build/app/php/post_file_to_s3.php
+++ www/build/app/php/post_file_to_s3.php
@@ -1,24 +1,24 @@
 <?php
 require('../../../../vendor/autoload.php');
-$s3 = Aws\S3\S3Client::factory();
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-  $file = $_POST["file"];
-  $dataFileName = $_POST["dataFileName"];
-  $dataFileType = $_POST["dataFileType"];
-  $bucket=getenv('bucket');
-try{
-  print_r($file['name']);
-  print_r($file['tmp_name']);
-  $result = $s3->putObject(array(
-    'Bucket' => $bucket,
-    'Key'    => $file['name'],
-    'Body'   => $file['tmp_name'],
-    'ACL'    => 'public-read'
-  ));
-}catch (S3Exception $e) {
-    echo $e->getMessage() . "\n";
-}
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  try{
+      $s3 = Aws\S3\S3Client::factory();
+      $file = $_POST["file"];
+      $dataFileName = $_POST["dataFileName"];
+      $dataFileType = $_POST["dataFileType"];
+      $bucket=getenv('bucket');
+      print_r($file['name']);
+      print_r($file['tmp_name']);
+      // $result = $s3->putObject(array(
+      //   'Bucket' => $bucket,
+      //   'Key'    => $file['name'],
+      //   'Body'   => $file['tmp_name'],
+      //   'ACL'    => 'public-read'
+      // ));
+  } catch (S3Exception $e) {
+      echo $e->getMessage() . "\n";
+  }
 
   // $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
 
