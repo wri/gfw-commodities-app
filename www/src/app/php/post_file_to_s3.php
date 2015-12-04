57e87870dee546505b6fd5ba2b0c8595a0417e73
Lucas Cotner <lcotner@blueraster.com>
2015-12-04 10:39:46 -0500

    Added proxy for Arc REST calls

--- www/src/app/php/post_file_to_s3.php
+++ www/src/app/php/post_file_to_s3.php
@@ -1,30 +1,45 @@
 <?php
 require('../../../../vendor/autoload.php');
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  try{
+  try {
       $s3 = Aws\S3\S3Client::factory();
+
+      $urls = "";
       $bucket=getenv('bucket');
-      $uploads = array();
       if(isset($_FILES['dataFile'])){
-        $upload = json_encode(uploadFile($bucket, $_FILES['dataFile']));
-        array_push($upload,$uploads);
-        print_r($upload);
+
+
+        $result = $s3->putObject(array(
+            'Bucket'       => $bucket,
+            'Key'          => $_FILES['dataFile']['name'],
+            'SourceFile'   => $_FILES['dataFile']['tmp_name'],
+            'ACL'          => 'public-read'
+        ));
+        $urls = $urls . $result['ObjectURL'];
+
       }
       if(isset($_FILES['attributeFile'])){
-        $upload2 = json_encode(uploadFile($bucket, $_FILES['attributeFile']));
-        array_push($upload2,$uploads);
+        //$upload = $s3->upload($bucket, $_FILES['attributeFile']['name'], fopen($_FILES['attributeFile']['tmp_name'], 'rb'), 'public-read');
+        //array_push($upload2,$uploads);
+
+        $result = $s3->putObject(array(
+            'Bucket'       => $bucket,
+            'Key'          => $_FILES['attributeFile']['name'],
+            'SourceFile'   => $_FILES['attributeFile']['tmp_name'],
+            'ACL'          => 'public-read'
+        ));
+        $urls = $urls . ";" . $result['ObjectURL'];
+
       }
+      print_r($urls);
 
   } catch (S3Exception $e) {
       echo "ERROR" . $e->getMessage() . "\n";
   }
+
 } else {
   echo("Sorry, you're not allowed to do this.");
 }
 
-function uploadFile($bucket,$file){
-    $upload = $s3->upload($bucket, $file['name'], fopen($file['tmp_name'], 'rb'), 'public-read');
-}
 
 ?>
