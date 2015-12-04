96af8187b97cd290dab37ad478ace67bb7766f79
Lucas Cotner <lcotner@blueraster.com>
2015-12-03 19:05:43 -0500

    Serializing php reponse into json again..........

--- www/build/app/php/post_file_to_s3.php
+++ www/build/app/php/post_file_to_s3.php
@@ -2,24 +2,28 @@
 require('../../../../vendor/autoload.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  try{
-      // $s3 = Aws\S3\S3Client::factory();
+  try {
+      $s3 = Aws\S3\S3Client::factory();
       print_r('finally');
-      // $bucket=getenv('bucket');
-      // $uploads = array();
-      // if(isset($_FILES['dataFile'])){
-      //   $upload = json_encode(uploadFile($bucket, $_FILES['dataFile']));
-      //   array_push($upload,$uploads);
-      //   print_r($upload);
-      // }
+      $bucket=getenv('bucket');
+      $uploads = array();
+      if(isset($_FILES['dataFile'])){
+        print_r('In the If');
+        $upload = json_encode(uploadFile($bucket, $_FILES['dataFile']));
+        array_push($upload,$uploads);
+        print_r($upload);
+      }else{
+        print_r("Never Made it in");
+      }
       // if(isset($_FILES['attributeFile'])){
       //   $upload2 = json_encode(uploadFile($bucket, $_FILES['attributeFile']));
       //   array_push($upload2,$uploads);
-      }
+      //}
 
   } catch (S3Exception $e) {
       echo "ERROR" . $e->getMessage() . "\n";
   }
+
 } else {
   echo("Sorry, you're not allowed to do this.");
 }
