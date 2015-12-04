d3f2513a730fd7370da21810736cb046e1f0bd34
Lucas Cotner <lcotner@blueraster.com>
2015-12-03 19:17:25 -0500

    Serializing php reponse into json again............

--- www/build/app/php/post_file_to_s3.php
+++ www/build/app/php/post_file_to_s3.php
@@ -6,12 +6,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       $s3 = Aws\S3\S3Client::factory();
       print_r('finally');
       $bucket=getenv('bucket');
-      $uploads = array();
       if(isset($_FILES['dataFile'])){
         $upload = $s3->upload($bucket, $_FILES['dataFile']['name'], fopen($_FILES['dataFile']['tmp_name'], 'rb'), 'public-read');
-        array_push($upload,$uploads);
-        print_r(json_encode($upload));
-        print_r(json_encode($uploads));
+        print_r($upload);
+
       }
       if(isset($_FILES['attributeFile'])){
         $upload = $s3->upload($bucket, $_FILES['attributeFile']['name'], fopen($_FILES['attributeFile']['tmp_name'], 'rb'), 'public-read');
