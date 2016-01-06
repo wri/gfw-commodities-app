a62d2cd60cf01a884bb03850d25143c7160f5d3b
Lucas Cotner <lcotner@blueraster.com>
2016-01-06 11:53:19 -0500

    Added in data submission page

--- www/src/app/bootloader.js
+++ www/src/app/bootloader.js
@@ -1,7 +1,7 @@
 /* global window, document, location */
 (function(win, doc) {
     'use strict';
-    var version = "2.5.31",
+    var version = "2.5.33",
         URL = location.pathname.replace(/\/[^/]+$/, "") + 'app',
         dojoConfig = {
             parseOnLoad: false,
--- www/src/app/js/utils/Loader.js
+++ www/src/app/js/utils/Loader.js
@@ -7,7 +7,7 @@ define([
 
         getTemplate: function(name) {
             var deferred = new Deferred(),
-                path = './app/templates/' + name + '.html?v=2.5.31',
+                path = './app/templates/' + name + '.html?v=2.5.33',
                 req;
 
             req = new XMLHttpRequest();
