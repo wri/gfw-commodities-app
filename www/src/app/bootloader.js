3c4fc9cfe2cfbbeb7bb0cd8685caf21d631d7688
Lucas Cotner <lcotner@blueraster.com>
2016-01-11 14:26:26 -0500

    Updated map controls style

--- www/src/app/bootloader.js
+++ www/src/app/bootloader.js
@@ -2,7 +2,7 @@
 (function(win, doc) {
     'use strict';
 
-    var version = "2.5.31",
+    var version = "2.5.33",
         URL = location.pathname.replace(/\/[^/]+$/, "") + 'app',
         dojoConfig = {
             parseOnLoad: false,
