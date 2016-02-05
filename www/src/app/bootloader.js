fc99574b93acd4428d8643c4917adf51bbf7771c
Lucas Cotner <lcotner@blueraster.com>
2016-02-05 16:12:15 -0500

    Resourced tree cover change, loss, and gain to the new WRI server

--- www/src/app/bootloader.js
+++ www/src/app/bootloader.js
@@ -1,7 +1,7 @@
 /* global window, document, location */
 (function(win, doc) {
     'use strict';
-    var version = "2.5.44",
+    var version = "2.5.45",
         URL = location.pathname.replace(/\/[^/]+$/, "") + 'app',
         dojoConfig = {
             parseOnLoad: false,
