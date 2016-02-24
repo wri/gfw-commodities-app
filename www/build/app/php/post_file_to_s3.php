ef6404f481412a66b3d423d3ec678bf5bd4a13ca
Lucas Cotner <lcotner@blueraster.com>
2016-02-24 13:14:05 -0500

    Adding to php requests

--- www/build/app/php/post_file_to_s3.php
+++ www/build/app/php/post_file_to_s3.php
@@ -6,6 +6,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
       $urls = "";
       $bucket=getenv('bucket');
+      $key=getenv('aws_access_key_id');
+      $secret=getenv('aws_secret_access_key');
+
+
       if(isset($_FILES['dataFile'])){
 
 
@@ -32,7 +36,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // $signedUrl = $s3->getObjectUrl($bucket, $_FILES['attributeFile']['name'], '+15 minutes');
 
       }
+
+      $client = Aws\Ses\SesClient::factory(array(
+          'key'    => $key,
+          'secret' => $secret,
+          'region' => 'us-east-1'
+      ));
+
+      $msg = array();
+      $msg['Source'] = "lcotner@blueraster.com";
+      //ToAddresses must be an array
+      $msg['Destination']['ToAddresses'][] = "jhettmansperger@blueraster.com";
+      $msg['Message']['Subject']['Data'] = "Email from S3 PHP SDK";
+      $msg['Message']['Subject']['Charset'] = "UTF-8";
+      $msg['Message']['Body']['Text']['Data'] ="The bodyyy";
+      $msg['Message']['Body']['Text']['Charset'] = "UTF-8";
+      $msg['Message']['Body']['Html']['Data'] ="<h3>HTML</h3> <p>Data of email</p>";
+      $msg['Message']['Body']['Html']['Charset'] = "UTF-8";
+
+      $client->sendEmail($msg);
+
       print_r($urls);
+
+
+
 
       // $obj = new stdClass();
       // $obj->URL = $urls;
--- www/src/app/js/utils/Loader.js
+++ www/src/app/js/utils/Loader.js
@@ -7,7 +7,7 @@ define([
 
         getTemplate: function(name) {
             var deferred = new Deferred(),
-                path = './app/templates/' + name + '.html?v=2.5.44',
+                path = './app/templates/' + name + '.html?v=2.5.45',
                 req;
 
             req = new XMLHttpRequest();
