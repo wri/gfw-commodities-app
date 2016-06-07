fff7da08e709deace7f1690d0e673c729aaff8b8
Lucas Cotner <lcotner@blueraster.com>
2016-06-07 14:06:19 -0700

    Build for prod push

--- www/src/app/js/utils/Loader.js
+++ www/src/app/js/utils/Loader.js
@@ -8,7 +8,7 @@ define([
 
         getTemplate: function(name) {
             var deferred = new Deferred(),
-                path = './app/templates/' + name + '.html?v=2.5.76',
+                path = './app/templates/' + name + '.html?v=2.5.77',
                 req;
 
             req = new XMLHttpRequest();
