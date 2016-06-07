59eb68bd53b082b3e781b1129b881396a3980f0f
Lucas Cotner <lcotner@blueraster.com>
2016-06-07 09:58:11 -0700

    Build with udpated mill api

--- www/src/app/bootloader.js
+++ www/src/app/bootloader.js
@@ -1,6 +1,6 @@
 /* global window, document, location */
 (function(win, doc) {
-    var version = '2.5.74',
+    var version = '2.5.75',
         URL = location.pathname.replace(/\/[^/]+$/, '') + 'app',
         dojoConfig = {
             parseOnLoad: false,
