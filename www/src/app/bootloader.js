e6f7733e0339709f48a449746a089ce02b2ed02c
Lucas Cotner <lc07@uw.edu>
2016-07-28 12:52:09 -0400

    Updated metadata IDs for certain layers

--- www/src/app/bootloader.js
+++ www/src/app/bootloader.js
@@ -1,6 +1,6 @@
 /* global window, document, location */
 (function(win, doc) {
-    var version = '2.6.7',
+    var version = '2.6.8',
         URL = location.pathname.replace(/\/[^/]+$/, '') + 'app',
         dojoConfig = {
             parseOnLoad: false,
--- www/src/app/js/utils/Loader.js
+++ www/src/app/js/utils/Loader.js
@@ -8,7 +8,7 @@ define([
 
         getTemplate: function(name) {
             var deferred = new Deferred(),
-                path = './app/templates/' + name + '.html?v=2.5.92',
+                path = './app/templates/' + name + '.html?v=2.5.93',
                 req;
 
             req = new XMLHttpRequest();
