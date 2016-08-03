6e4a327519caf5b8f57e90142e4e0e233676322d
Robert Winterbottom <rwinterbottom@blueraster.com>
2016-08-03 09:15:46 -0400

    New build with version 2.4.3

--- www/build/app/js/bundle.js
+++ www/build/app/js/bundle.js
@@ -13437,7 +13437,7 @@ define('utils/Loader',[
 
         getTemplate: function(name) {
             var deferred = new Deferred(),
-                path = './app/templates/' + name + '.html?v=2.5.93',
+                path = './app/templates/' + name + '.html?v=2.4.3',
                 req;
 
             req = new XMLHttpRequest();
@@ -13463,7 +13463,6 @@ define('utils/Loader',[
             });
 
             // esri.config.defaults.io.corsEnabledServers.push('54.88.79.102');
-            console.log('added proxyUrl!');
             var layersRequest = esriRequest({
                 url: path,
                 handleAs: 'json',
