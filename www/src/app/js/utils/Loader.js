5d2742cf55ce9f9e922bcf8c2ddc4d58e68cf69f
Robert Winterbottom <rwinterbottom@blueraster.com>
2016-08-05 13:59:45 -0400

    New mill API Updated version numbers

--- www/src/app/js/utils/Loader.js
+++ www/src/app/js/utils/Loader.js
@@ -8,7 +8,7 @@ define([
 
         getTemplate: function(name) {
             var deferred = new Deferred(),
-                path = './app/templates/' + name + '.html?v=2.4.3',
+                path = './app/templates/' + name + '.html?v=2.4.4',
                 req;
 
             req = new XMLHttpRequest();
