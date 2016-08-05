54086c6c71486b9be2a4f1f5a178b7403b4f3036
Robert Winterbottom <rwinterbottom@blueraster.com>
2016-08-05 14:02:13 -0400

    New build

--- www/build/app/js/bundle.js
+++ www/build/app/js/bundle.js
@@ -13437,7 +13437,7 @@ define('utils/Loader',[
 
         getTemplate: function(name) {
             var deferred = new Deferred(),
-                path = './app/templates/' + name + '.html?v=2.4.3',
+                path = './app/templates/' + name + '.html?v=2.4.4',
                 req;
 
             req = new XMLHttpRequest();
