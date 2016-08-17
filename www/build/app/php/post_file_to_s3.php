4b28673b116d47d5b0cb6209734d86bed303e892
Lucas Cotner <lc07@uw.edu>
2016-08-17 09:32:27 -0400

    Updating data submission

--- www/build/app/php/post_file_to_s3.php
+++ www/build/app/php/post_file_to_s3.php
@@ -14,33 +14,46 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       ));
 
 
-
       $urls = "";
       $bucket=getenv('bucket');
-      if(isset($_FILES['dataFile'])){
+      if(isset($_FILES['concessionFile'])){
 
+        $concessionFileName = $_POST['storyCompany'] . "/concessions/" . $_FILES['concessionFile']['name'];
+        // 'Key'          => $_FILES['concessionFile']['name'],
         $result = $s3->putObject(array(
             'Bucket'       => $bucket,
-            'Key'          => $_FILES['dataFile']['name'],
-            'Body'   => $_FILES['dataFile']['tmp_name'],
+            'Key'          => $concessionFileName,
+            'Body'   => $_FILES['concessionFile']['tmp_name'],
             'ACL'          => 'public-read'
         ));
         $urls = $urls . $result['ObjectURL'];
-
       }
-      if(isset($_FILES['attributeFile'])){
+      if(isset($_FILES['facilityFile'])){
+
+        $facilityFileName = $_POST['storyCompany'] . "/concessions/" . $_FILES['facilityFile']['name'];
 
         $result = $s3->putObject(array(
             'Bucket'       => $bucket,
-            'Key'          => $_FILES['attributeFile']['name'],
-            'Body'   => $_FILES['attributeFile']['tmp_name'],
+            'Key'          => $facilityFileName,
+            'Body'   => $_FILES['facilityFile']['tmp_name'],
             'ACL'          => 'public-read'
         ));
         $urls = $urls . ";" . $result['ObjectURL'];
+      }
+      if(isset($_FILES['otherFile'])){
 
+        $otherFileName = $_POST['storyCompany'] . "/concessions/" . $_FILES['otherFile']['name'];
+
+        $result = $s3->putObject(array(
+            'Bucket'       => $bucket,
+            'Key'          => $otherFileName,
+            'Body'   => $_FILES['otherFile']['tmp_name'],
+            'ACL'          => 'public-read'
+        ));
+        $urls = $urls . ";" . $result['ObjectURL'];
       }
 
-      $messageBody = "<p>The file " . $_FILES['dataFile']['name'] . " was uploaded by " . $_POST['storyUserName'] . ".</p><p>They can be reached at <a href='" . $_POST['storyEmail'] . "'>" . $_POST['storyEmail'] . "</a>.</p>";
+      $messageBody = "<p>The file " . $_FILES['concessionFile']['name'] . " was uploaded by " . $_POST['storyUserName'] . ".</p><p>They can be reached at <a href='" . $_POST['storyEmail'] . "'>" . $_POST['storyEmail'] . "</a>.</p>";
 
       $email_result = $sesClient->sendEmail(array(
           // Source is required
@@ -74,7 +87,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
           )
       ));
 
-      print_r($_POST['dataFileName']);
+      print_r($_POST['concessionFileName']);
       // print_r($credentials);
       // print_r($email_result);
       // print_r($urls);
