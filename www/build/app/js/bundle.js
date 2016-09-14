8f6c4683ddb7ace44cca12179b3ae7d22b162818
Lucas Cotner <lc07@uw.edu>
2016-09-14 10:33:40 -0400

    Updating staging

--- www/build/app/js/bundle.js
+++ www/build/app/js/bundle.js
@@ -577,7 +577,8 @@ define('map/config',[], function() {
         granChaco: {
             id: 'granChaco',
             url: granChacoUrl,
-            defaultLayers: [0]
+            defaultLayers: [0],
+            toolsNode: 'guyra_toolbox'
         },
         loss: {
             id: 'Loss',
@@ -4790,9 +4791,10 @@ define('components/wizard/StepOne',[
 
   // Helper Functions
   function getDefaultState() {
+
     return {
       completed: true,
-      selectedOption: WizardStore.get(KEYS.areaOfInterest) || option3.id,
+      selectedOption: WizardStore.get(KEYS.areaOfInterest) || option1.id,
       previousAreaOfInterest: undefined
     };
   }
@@ -4822,9 +4824,9 @@ define('components/wizard/StepOne',[
       // Set the default value in the store
 			var aoi = WizardStore.get(KEYS.areaOfInterest);
 			console.log(aoi);
-			console.log(option3.id);
+
 			if (!aoi) {
-				WizardActions.setAreaOfInterest(option3.id);
+				WizardActions.setAreaOfInterest(option1.id);
 			}
       // Register a callback to the item of interest
       WizardStore.registerCallback(KEYS.areaOfInterest, this.areaOfInterestUpdated);
@@ -7219,7 +7221,7 @@ define('map/LayerController',[
 
                 console.log(props);
                 console.log(layer);
-                
+
 
             dojoQuery('.gfw .filter-list .' + queryClass).forEach(function(node) {
                 itemLayer = node.dataset ? node.dataset.layer : node.getAttribute('data-layer');
@@ -7235,28 +7237,9 @@ define('map/LayerController',[
             if (layer) {
 
                 if (visibleLayers.length > 0) {
-                    //if (layer.visibleLayers.length > 0) {
-
-                        // for (var i = 0; i < layer.visibleLayers.length; i++) {
-                        //     if (visibleLayers.indexOf(layer.visibleLayers[i]) == -1 && layer.visibleLayers[i] != 0) {
-                        //         console.log(layer.visibleLayers[i]);
-                        //         debugger
-                        //         visibleLayers.push(layer.visibleLayers[i]);
-                        //     }
-                        // }
-
-                        // if (visibleLayers.indexOf(layer.visibleLayers[0]) == -1) {
-                        //     debugger
-                        //     visibleLayers.push(layer.visibleLayers[0]);
-                        // }
-                    //}
-
                     layer.setVisibleLayers(visibleLayers);
                     layer.show();
-                    console.log(layer.visibleLayers);
                 } else {
-                    console.log('hiding');
-                    console.log(layer.visibleLayers);
                     layer.hide();
                 }
                 this.refreshLegendWidget();
@@ -7371,6 +7354,20 @@ define('map/LayerController',[
               return false;
           }
           return ((year % 100) !== 0 || (year % 400) === 0);
+        },
+
+        updateGuyraDates: function(clauseArray) {
+          var start = clauseArray[0];
+          var end = clauseArray[1];
+
+          var guyraLayer = app.map.getLayer('granChaco');
+          var layerDefs = [];
+          var where = "date >= '" + start.toDateString() + "' AND date <= '" + end.toDateString() + "'";
+          console.log(where);
+          layerDefs[0] = where;
+          guyraLayer.setLayerDefinitions(layerDefs);
+          console.log(guyraLayer);
+
         },
 
         updateGladDates: function(clauseArray) {
@@ -8659,6 +8656,7 @@ define('map/ProdesSlider',[
     });
 
     request.then(function (res) {
+      console.log('res', res);
       // Labels should be formatted like so: {month|numeric} - {year|two-digit}
       var min = res.minValues[0],
           max = res.maxValues[0],
@@ -8767,6 +8765,169 @@ define('map/ProdesSlider',[
 
 });
 
+define('map/GuyraSlider',[
+  'dojo/on',
+  'map/config',
+  'esri/tasks/query',
+  'esri/tasks/QueryTask',
+  'dojo/Deferred',
+  'utils/Analytics',
+  'map/LayerController'
+], function (on, MapConfig, Query, QueryTask, Deferred, Analytics, LayerController) {
+
+  var playInterval,
+      guyraSlider,
+      playButton;
+
+  var config = {
+    sliderSelector: '#guyra-alert-slider',
+    playHtml: '&#9658;',
+    pauseHtml: '&#x25A0',
+    baseYear: 1999
+  };
+
+  var state = {
+    isPlaying: false,
+    from: 0,
+    to: 0
+  };
+
+  var getGuyraLabels = function getGuyraLabels () {
+    var deferred = new Deferred(),
+        query, queryTask;
+
+    query = new Query();
+    query.returnGeometry = false;
+    query.outFields = ['date'];
+    query.returnDistinctValues = true;
+    query.where = '1=1';
+
+    queryTask = new QueryTask(MapConfig.granChaco.url + '/' + MapConfig.granChaco.defaultLayers[0]);
+
+    queryTask.execute(query, function(res) {
+      var labels = [], date, month, year;
+
+      res.features.sort(function(a, b){
+        return new Date(b.attributes.date) - new Date(a.attributes.date);
+      });
+
+      res.features.reverse();
+
+      // var max = new Date(res.features[0].attributes.date);
+      // var min = new Date(res.features[res.features.length - 1].attributes.date);
+
+      for (var j = 0; j < res.features.length; j++) {
+        date = new Date(res.features[j].attributes.date);
+        year = date.getFullYear().toString().substr(2, 2);
+        month = ('0' + (date.getMonth() + 1)).slice(-2);
+        labels.push(month + '-' + year);
+
+      }
+      deferred.resolve(labels);
+
+    });
+
+    return deferred;
+  };
+
+  var GuyraSlider = {
+
+    init: function () {
+      var self = this;
+      if (guyraSlider === undefined) {
+        getGuyraLabels().then(function (labels) {
+          console.log(labels);
+          $(config.sliderSelector).ionRangeSlider({
+            type: 'double',
+            values: labels,
+            grid: true,
+            prettify_enabled: false,
+            hide_min_max: true,
+            hide_from_to: true,
+            onFinish: self.change,
+            onUpdate: self.change
+          });
+          // Save this instance to a variable ???
+          guyraSlider = $(config.sliderSelector).data('ionRangeSlider');
+          // Cache query for play button
+          playButton = $('#guyraPlayButton');
+          // Attach Events related to this item
+          on(playButton, 'click', self.playToggle);
+          //- set the state for change tracking
+          state.to = labels.length - 1;
+        });
+      }
+    },
+
+    change: function (data) {
+
+      var fromData = data.from_value.split('-');
+      var toData = data.to_value.split('-');
+
+      var fromDate = new Date(fromData[0] + '/1/20' + fromData[1]);
+      var toDate = new Date(toData[0] + '/1/20' + toData[1]);
+
+      LayerController.updateGuyraDates([fromDate, toDate]);
+      //- Determine which handle changed and emit the appropriate event
+      if (!state.isPlaying) {
+        if (data.from !== state.from) {
+          Analytics.sendEvent('Event', 'Forma Timeline', 'Change start date');
+        } else {
+          Analytics.sendEvent('Event', 'Forma Timeline', 'Change end date');
+        }
+      }
+      //- Update the state value
+      state.from = fromDate;
+      state.to = toDate;
+    },
+
+    playToggle: function () {
+      var fromValue, toValue, endValue;
+
+      function stopPlaying() {
+        state.isPlaying = false;
+        clearInterval(playInterval);
+        playButton.html(config.playHtml);
+      }
+
+      if (state.isPlaying) {
+        stopPlaying();
+      } else {
+        // Update some state
+        state.isPlaying = true;
+        endValue = guyraSlider.result.to;
+        // Trigger a change on the layer for the initial value, with both handles starting at the same point
+        guyraSlider.update({ from: guyraSlider.result.from, to: guyraSlider.result.from });
+        // Start the interval
+        playInterval = setInterval(function () {
+          // We will be incrementing the from value to move the slider forward
+          fromValue = guyraSlider.result.from;
+          toValue = guyraSlider.result.to;
+          // Quit if from value is equal to or greater than the to value
+          if (toValue >= endValue) {
+            stopPlaying();
+          } else {
+            // Update the slider
+            guyraSlider.update({
+              from: fromValue,
+              to: ++toValue
+            });
+          }
+
+        }, 1250);
+
+        // Update the button html
+        playButton.html(config.pauseHtml);
+      }
+      Analytics.sendEvent('Event', 'Guyra Timeline', 'Play');
+    }
+
+  };
+
+  return GuyraSlider;
+
+});
+
 define('map/Controls',[
     'dojo/dom',
     'dojo/query',
@@ -8784,12 +8945,13 @@ define('map/Controls',[
     'map/FormaSlider',
     'map/GladSlider',
     'map/ProdesSlider',
+    'map/GuyraSlider',
     'map/LayerController',
     'esri/request',
     'esri/TimeExtent',
     'esri/dijit/TimeSlider',
     'dijit/form/CheckBox'
-], function(dom, dojoQuery, Deferred, Fx, arrayUtils, domClass, domStyle, registry, domConstruct, Hasher, MapConfig, MapModel, LossSlider, FormaSlider, GladSlider, ProdesSlider, LayerController, request, TimeExtent, TimeSlider, Checkbox) {
+], function(dom, dojoQuery, Deferred, Fx, arrayUtils, domClass, domStyle, registry, domConstruct, Hasher, MapConfig, MapModel, LossSlider, FormaSlider, GladSlider, ProdesSlider, GuyraSlider, LayerController, request, TimeExtent, TimeSlider, Checkbox) {
 
 
     var jq171 = jQuery.noConflict();
@@ -8960,6 +9122,7 @@ define('map/Controls',[
             FormaSlider.init();
             GladSlider.init();
             ProdesSlider.init();
+            GuyraSlider.init();
         },
 
         // fetchFORMAAlertsLabels: function() {
@@ -9826,7 +9989,7 @@ define('components/wizard/Wizard',[
             });
 
             // Reset this components state
-            WizardStore.set(KEYS.areaOfInterest, AnalyzerConfig.stepOne.option3.id, true);
+            WizardStore.set(KEYS.areaOfInterest, AnalyzerConfig.stepOne.option1.id, true);
             WizardStore.set(KEYS.selectedCustomFeatures, [], true);
             WizardStore.set(KEYS.userStep, 0, true);
             this.replaceState(getDefaultState());
@@ -13438,7 +13601,7 @@ define('utils/Loader',[
 
         getTemplate: function(name) {
             var deferred = new Deferred(),
-                path = './app/templates/' + name + '.html?v=2.4.10',
+                path = './app/templates/' + name + '.html?v=2.4.11',
                 req;
 
             req = new XMLHttpRequest();
