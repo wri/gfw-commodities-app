8e28378d30562609f6959c7b4d53f44d1f8fcf00
Lucas Cotner <lc07@uw.edu>
2016-11-07 12:22:44 -0500

    Updating ie-specific querySelectors

--- www/src/app/js/utils/Loader.js
+++ www/src/app/js/utils/Loader.js
@@ -8,7 +8,7 @@ define([
 
         getTemplate: function(name) {
             var deferred = new Deferred(),
-                path = './app/templates/' + name + '.html?v=2.4.21',
+                path = './app/templates/' + name + '.html?v=2.4.22',
                 req;
 
             req = new XMLHttpRequest();
