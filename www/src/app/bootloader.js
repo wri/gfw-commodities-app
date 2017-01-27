a148cff223752e313d8f497eff2421530d3197ba
Lucas Cotner <lc07@uw.edu>
2017-01-27 08:43:28 -0500

    New build for GLAD tests

--- www/src/app/bootloader.js
+++ www/src/app/bootloader.js
@@ -1,6 +1,6 @@
 /* global window, document, location */
 (function(win, doc) {
-    var version = '2.4.50',
+    var version = '2.4.51',
         URL = location.pathname.replace(/\/[^/]+$/, '') + 'app',
         dojoConfig = {
             parseOnLoad: false,
