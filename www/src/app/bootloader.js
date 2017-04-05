282282d9bfc3a93ddfe77eaa950850f927bfa44b
Lucas Cotner <lc07@uw.edu>
2017-04-05 17:04:00 -0400

    Updated staging with canvasLayer request queue

--- www/src/app/bootloader.js
+++ www/src/app/bootloader.js
@@ -1,6 +1,6 @@
 /* global window, document, location */
 (function(win, doc) {
-    var version = '2.4.65',
+    var version = '2.4.66',
         URL = location.pathname.replace(/\/[^/]+$/, '') + 'app',
         dojoConfig = {
             parseOnLoad: false,
--- www/src/app/js/utils/Loader.js
+++ www/src/app/js/utils/Loader.js
@@ -8,7 +8,7 @@ define([
 
         getTemplate: function(name) {
             var deferred = new Deferred(),
-                path = './app/templates/' + name + '.html?v=2.4.65',
+                path = './app/templates/' + name + '.html?v=2.4.66',
                 req;
 
             req = new XMLHttpRequest();
