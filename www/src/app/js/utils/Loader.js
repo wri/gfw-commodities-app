038a69a013b43acfcbe5aab15384a1316dede58b
Lucas Cotner <lc07@uw.edu>
2017-04-10 14:22:56 -0400

    Updated soy to latest 30m resolution layer

--- www/src/app/js/utils/Loader.js
+++ www/src/app/js/utils/Loader.js
@@ -8,7 +8,7 @@ define([
 
         getTemplate: function(name) {
             var deferred = new Deferred(),
-                path = './app/templates/' + name + '.html?v=2.4.67',
+                path = './app/templates/' + name + '.html?v=2.4.68',
                 req;
 
             req = new XMLHttpRequest();
