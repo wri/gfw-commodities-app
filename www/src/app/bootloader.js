6d6cc56833b5dc847f452908bdd43c991cb6321d
Lucas Cotner <lc07@uw.edu>
2017-06-29 13:23:16 -0400

    Latest build with old server refs removed

--- www/src/app/bootloader.js
+++ www/src/app/bootloader.js
@@ -1,6 +1,6 @@
 /* global window, document, location */
 (function(win, doc) {
-    var version = '2.4.85',
+    var version = '2.4.86',
         URL = location.pathname.replace(/\/[^/]+$/, '') + 'app',
         dojoConfig = {
             parseOnLoad: false,
--- www/src/app/js/utils/Loader.js
+++ www/src/app/js/utils/Loader.js
@@ -8,7 +8,7 @@ define([
 
         getTemplate: function(name) {
             var deferred = new Deferred(),
-                path = './app/templates/' + name + '.html?v=2.4.85',
+                path = './app/templates/' + name + '.html?v=2.4.86',
                 req;
 
             req = new XMLHttpRequest();
