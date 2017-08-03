30a2f48c833b94c8e387cfb5237529c683272111
Lucas Cotner <lc07@uw.edu>
2017-08-03 10:55:20 -0400

    Pointing our mill-api rasterIds to their latest features

--- www/src/app/js/utils/Loader.js
+++ www/src/app/js/utils/Loader.js
@@ -8,7 +8,7 @@ define([
 
         getTemplate: function(name) {
             var deferred = new Deferred(),
-                path = './app/templates/' + name + '.html?v=2.4.91',
+                path = './app/templates/' + name + '.html?v=2.4.92',
                 req;
 
             req = new XMLHttpRequest();
