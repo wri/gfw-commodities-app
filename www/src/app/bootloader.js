c7144dc169fada455410cf5a442ac306f0f26b8e
Lucas Cotner <lc07@uw.edu>
2017-11-27 08:20:15 -0500

    Added 'page' prop to liveSettings

--- www/src/app/bootloader.js
+++ www/src/app/bootloader.js
@@ -1,6 +1,6 @@
 /* global window, document, location */
 (function(win, doc) {
-    var version = '2.5.0',
+    var version = '2.5.1',
         URL = location.pathname.replace(/\/[^/]+$/, '') + 'app',
         dojoConfig = {
             parseOnLoad: false,
--- www/src/app/js/utils/Loader.js
+++ www/src/app/js/utils/Loader.js
@@ -8,7 +8,7 @@ define([
 
         getTemplate: function(name) {
             var deferred = new Deferred(),
-                path = './app/templates/' + name + '.html?v=2.5.0',
+                path = './app/templates/' + name + '.html?v=2.5.1',
                 req;
 
             req = new XMLHttpRequest();
