95c733c3737764e1a969a88fb442e65b3c1509d2
Lucas Cotner <lc07@uw.edu>
2017-11-30 14:13:42 -0500

    Higher php version

--- www/src/app/js/utils/Loader.js
+++ www/src/app/js/utils/Loader.js
@@ -8,7 +8,7 @@ define([
 
         getTemplate: function(name) {
             var deferred = new Deferred(),
-                path = './app/templates/' + name + '.html?v=2.5.2',
+                path = './app/templates/' + name + '.html?v=2.5.4',
                 req;
 
             req = new XMLHttpRequest();
